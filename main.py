84a59cb039eac384b89385e4822802b83f0bc80d
Hollitizz <valentin.caure@epitech.eu>
2024-03-05 12:14:14 +0100

    add: enfile

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from utils import loggingConfig
 import dotenv
 
 if __name__ == "__main__":
+    dotenv.load_dotenv()
     loggingConfig.setupLogging()
     # import routes
     from routes import app
 
-    dotenv.load_dotenv()
     app.run(debug=os.getenv("ENV") != "production")
--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,6 @@ from routes import app
 
 if __name__ == "__main__":
     loggingConfig.setupLogging()
+    app.logger.info("Starting")
 
     app.run()
