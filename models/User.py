56c92a0e4e534a656155836f139e4a091b48c680
Stiaez <cesar-stevens.gonzalez-arango@epitech.eu>
2024-03-05 16:05:22 +0100

    refactor : User model hide password when serialized

--- models/User.py
+++ models/User.py
@@ -7,10 +7,10 @@ class User(BaseModel):
         arbitrary_types_allowed = True
 
     id: ObjectId = Field(default=None, alias="_id")
-    username: str = None
-    password: str = None
-    email: str = None
-    token: str = None
+    username: str
+    password: str = Field(default=None)
+    email: str
+    token: str
 
     def model_dump(self):
-        return super().model_dump(by_alias=True, exclude_unset=True)
+        return super().model_dump(by_alias=True, exclude_unset=True, exclude={"password"})
