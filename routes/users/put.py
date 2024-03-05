5b5ef44f8817c0707bc24ffc32b0a390216ae699
minhduc <ducbv1407@gmail.com>
2024-03-05 15:36:39 +0100

    add: update user (put) route

---
+++ routes/users/put.py
@@ -0,0 +1,18 @@
+from flask import request
+from bson import ObjectId
+from models.User import User
+from repositories import user_repository
+
+
+def update(user_id: str):
+    _user_id = ObjectId(user_id)
+
+    username = request.json.get('username')
+    password = request.json.get('password')
+    email = request.json.get('email')
+
+    new_user = User(username=username, password=password, email=email)
+
+    updated_user = user_repository.updateUser(_user_id, new_user)
+
+    return updated_user.model_dump()
