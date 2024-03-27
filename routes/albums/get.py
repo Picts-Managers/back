02f6983438375c65d4d46d98ce0ea7809aee5674
Hollitizz <valentin.caure@epitech.eu>
2024-03-27 23:24:16 +0100

    Fix album cover ID in get_albums() function

--- routes/albums/get.py
+++ routes/albums/get.py
@@ -1,4 +1,5 @@
 from flask import request
+
 from middlewares import isLogged, schema
 from repositories import album_repository
 from schemas.albums import getAlbums
@@ -9,7 +10,10 @@ from utils import route
 @isLogged
 @schema(getAlbums)
 def get_albums():
-    albums = album_repository.getOwnedAlbums(request.req_user.id)
+    albums = album_repository.getMyAlbums(request.req_user.id)
+    response = []
     for album in albums:
-        album.cover_id = album.pictures_ids[0] if len(album.pictures_ids) else None
-    return {"albums": albums}
+        tmp = dict(album)
+        tmp["cover_ide"] = album.pictures_ids[0] if len(album.pictures_ids) else None
+        response.append(tmp)
+    return {"albums": response}
