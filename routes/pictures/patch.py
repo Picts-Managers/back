9c6c0d56a51021a57b85df639bb41730dd5dcab3
Stiaez <cesar-stevens.gonzalez-arango@epitech.eu>
2024-04-09 23:24:12 +0200

    add: getUserByEmail for share picture

--- routes/pictures/patch.py
+++ routes/pictures/patch.py
@@ -3,7 +3,7 @@ from flask import request
 
 from middlewares import schema
 from middlewares.auth import isLogged
-from repositories import picture_repository
+from repositories import picture_repository, user_repository
 from schemas.pictures import sharePicture
 from utils.routeDecorator import route
 
@@ -12,7 +12,9 @@ from utils.routeDecorator import route
 @isLogged
 @schema(sharePicture)
 def share_picture(picture_id):
-    target_user_id = ObjectId(request.body.user)
+    target_user_email = request.body.email
+    target_user = user_repository.getUserByEmail(target_user_email)
+
     picture_id = ObjectId(picture_id)
-    picture = picture_repository.share(picture_id, target_user_id)
+    picture = picture_repository.share(picture_id, target_user.id)
     return picture
