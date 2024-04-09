44a61b6cd107ec995997e89c250d6345f22757a0
Hollitizz <valentin.caure@epitech.eu>
2024-04-10 00:12:57 +0200

    Chg/matchDoc (#16)
    * update to match the doc
    
    * add doc

--- routes/users/patch.py
+++ routes/users/patch.py
@@ -2,27 +2,22 @@ from flask import request
 
 from middlewares import schema
 from middlewares.auth import isLogged
-from schemas.users import getUser
+from models.User import User
+from repositories import user_repository
+from schemas.users import userUpdate
 from utils import route
 
 
 @route("/me")
 @isLogged
-@schema(getUser)
+@schema(userUpdate)
 def update():
-    # _user_id = ObjectId(user_id)
-    # # new_user = User()
+    username = (
+        request.body.username if request.body.username else request.req_user.username
+    )
+    email = request.body.email if request.body.email else request.req_user.email
+    new_user = User(username=username, email=email)
 
-    # body = request.json
-    # match body:
-    #     case "username":
-    #         new_user = User(username=body)
-    #     case "password":
-    #         new_user = User(password=body)
-    #     case "email":
-    #         new_user = User(email=body)
-
-    # updated_user = user_repository.updateUser(_user_id, new_user)
-    # del updated_user.password
-    # return updated_user
-    return request.req_user
+    updated_user = user_repository.updateUser(request.req_user, new_user)
+    del updated_user.password
+    return updated_user
