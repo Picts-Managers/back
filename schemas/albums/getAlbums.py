596aa33ff6a4f6436c65f7037b0795b719e36049
Stiaez <cesar-stevens.gonzalez-arango@epitech.eu>
2024-04-22 13:49:01 +0200

    add: picturesList into AlbumResponse

--- schemas/albums/getAlbums.py
+++ schemas/albums/getAlbums.py
@@ -1,6 +1,26 @@
+import datetime
 from typing import Optional
+from bson import ObjectId
+from pydantic import BaseModel, field_validator
+from utils.types import Coordinates
 
-from pydantic import BaseModel
+
+class _ResPictures(BaseModel):
+    id: str
+    filename: str
+    owner_id: str
+    date: str = None
+    is_fav: bool = False
+    location: Optional[Coordinates] = None
+    viewers_ids: list[str] = []
+
+    @field_validator("date")
+    def validate_date(cls, value):
+        if value is None:
+            return ObjectId(cls.id).generation_time.isoformat()
+        if isinstance(value, datetime.datetime):
+            return value.isoformat()
+        return value
 
 
 class _ResAlbum(BaseModel):
@@ -9,7 +29,7 @@ class _ResAlbum(BaseModel):
     cover_id: Optional[str] = None
     title: str
     viewers_ids: list[str] = []
-    pictures_ids: list[str] = []
+    pictures: list[_ResPictures] = []
 
 
 class Response(BaseModel):
