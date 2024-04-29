7f18573d1212ccf984d9882f3704dd550daf023f
minhduc <ducbv1407@gmail.com>
2024-04-29 17:15:43 +0200

    fix: change method name

--- utils/auth.py
+++ utils/auth.py
@@ -10,8 +10,8 @@ ALGORITHM = os.getenv("ALGORITHM")
 def encrypt_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
-def verify_password(password: str, hashed_password: bytes) -> bool:
-    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+def verify_password(password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode())
 
 def generate_token(id: ObjectId) -> str:
     return jwt.encode({"id": str(id)}, SECRET_KEY, algorithm=ALGORITHM)
