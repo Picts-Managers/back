a08c323975a139f8ffdc28f902e35ccd94c75619
Hollitizz <valentin.caure@epitech.eu>
2024-05-08 21:58:54 +0200

    Fix import statement and improve test setup in conftest.py

--- tests/test_routes/conftest.py
+++ tests/test_routes/conftest.py
@@ -10,6 +10,7 @@ if not os.getenv("IS_CI"):
     from utils import app, db
 else:
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    print(sys.path)
     import routes
     from utils import app, db
 
