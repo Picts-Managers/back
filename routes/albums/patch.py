50d1d222e7fdd219ceb27223574331763bdd9cf3
Hollitizz <valentin.caure@epitech.eu>
2024-05-13 16:16:09 +0200

    add some test

--- routes/albums/patch.py
+++ routes/albums/patch.py
@@ -1,15 +1,11 @@
-from datetime import datetime
-
 from bson import ObjectId
 from flask import Blueprint, abort, request
-from PIL import Image
 
 from middlewares import schema
 from middlewares.auth import isLogged
-from models import Picture
 from repositories import album_repository, picture_repository, user_repository
 from schemas.albums import addPictureToAlbum, shareAlbum
-from utils.image import get_metadata
+from utils.image import handle_picture_upload
 
 blueprint = Blueprint(__name__.replace(".", "/"), __name__)
 
@@ -46,29 +42,7 @@ def add_picture_in_album(album_id: str):
 @isLogged
 def upload_picture_to_album(album_id: str):
     uploaded_file = request.files["file"]
-    image = Image.open(uploaded_file)
-    filename = ".".join(uploaded_file.filename.split(".")[:-1])
-    metadata = get_metadata(image)
-    picture = Picture(
-        owner_id=request.req_user.id,
-        location=metadata.get("location", None),
-        date=metadata.get("date", datetime.now().isoformat()),
-        filename=filename,
-        mimetype=uploaded_file.mimetype,
-    )
-    picture = picture_repository.insertPicture(picture)
-
-    if image.mode == "RGBA" or image.mode == "P":
-        if image.mode == "P":
-            image = image.convert("RGBA")
-
-        white_image = Image.new("RGBA", image.size, "WHITE")
-        white_image.paste(image, mask=image)
-        image = white_image.convert("RGB")
-
-    image.save(f"uploads/{picture.id}", "JPEG")
-    image.thumbnail((200, 200))
-    image.save(f"uploads/{picture.id}.low", "JPEG")
+    picture = handle_picture_upload(uploaded_file)
 
     album_id = ObjectId(album_id)
     album = album_repository.addPicture(
